ce7a5b03ec51737b92df2e315e38051c69228702
lukuang <lukuang1989@gmail.com>
2015-09-14 11:11:56 -0400

    detect queries without query string

--- process_log.py
+++ process_log.py
@@ -8,6 +8,7 @@ import argparse
 
 
 def read_log(log_file):
+    logs = {}
     with open(log_file) as f:
         for line in f:
             if "/solr/collection1/browse" not in line:
@@ -16,6 +17,24 @@ def read_log(log_file):
             m = re.search("^(.+?)\s+-\s+-\s+\[(.+?)\] \"(.+?)\"",line)
             if m is not None:
                 ip,complex_time, action = m.group(1),m.group(2),m.group(3)
+                mq = re.search("browse\?\&?q\=(.+?)\&?HTTP/1\.1",action)
+                if mq is None:
+                    print "skip no query actions"
+                    print line
+                    #raw_input("press enter to continue")
+                    continue
+                q_filed = mq.group(1)
+                print type(q_filed)
+                query_string = ""
+                mqs = re.search("(Id=\d+\+)?(\S+)",q_filed)
+                #if mqs is not None:
+
+                #if "mlt=true" in q_filed:
+                #
+                #elif "relevent=" in q_filed:
+                #
+                #else:
+
                 mt = re.match("(.+)\s(\+|\-)(\d{2})(\d{2})", complex_time)
                 if mt is not None:
                     t =  int(time.mktime(time.strptime(mt.group(1),"%d/%b/%Y:%H:%M:%S") ))
@@ -24,10 +43,12 @@ def read_log(log_file):
                         offset *= -1
                     t += offset
                     print ip,t
+
+
                 else:
                     print "error time format"
                     print line
-                #raw_input("press enter to continue")
+                raw_input("press enter to continue")
             else:
                 print "error line!"
                 print line
