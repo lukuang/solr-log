572f8a393ef626f8c2390d1f6596e40c67a83d80
lukuang <lukuang1989@gmail.com>
2015-09-14 10:11:01 -0400

    successfully get epoch time stamp

--- process_log.py
+++ process_log.py
@@ -12,8 +12,23 @@ def read_log(log_file):
         for line in f:
             m = re.search("^(.+?)\s+-\s+-\s+\[(.+?)\] \"(.+?)\"",line)
             if m is not None:
-                print m.group(1),m.group(2),m.group(3)
-            raw_input("press enter to continue")
+                ip,complex_time, action = m.group(1),m.group(2),m.group(3)
+                mt = re.match("(.+)\s(\+|\-)(\d{2})(\d{2})", complex_time)
+                if mt is not None:
+                    t =  int(time.mktime(time.strptime(mt.group(1),"%d/%b/%Y:%H:%M:%S") ))
+                    offset = int( mt.group(3) )*3600 + int( mt.group(4) )*60
+                    if mt.group(2) == "+":
+                        offset *= -1
+                    t += offset
+                    print ip,t
+                else:
+                    print "error time format"
+                    print line
+                raw_input("press enter to continue")
+            else:
+                print "error line!"
+                print line
+            #raw_input("press enter to continue")
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
